5b53245c89cafcdf0c92b21cdd01defb8bd9d314
Mizoko <mizoko320@gmail.com>
2022-03-19 09:06:42 +0900

    footerの作成

---
+++ portfolio/components/footer.js
@@ -0,0 +1,9 @@
+import styles from './footer.module.sass'
+
+export default function Footer() {
+  return (
+    <header className={styles.footer}>
+      <small>copyright 2022</small>
+    </header>
+  )
+}
--- portfolio/pages/index.js
+++ portfolio/pages/index.js
@@ -2,6 +2,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.sass'
 import Header from '../components/header'
+import Footer from '../components/footer'
 
 export default function Home() {
   return (
@@ -16,18 +17,7 @@ export default function Home() {
         <p>メインコンテンツ</p>
       </main>
 
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
+      <Footer />
     </div>
   )
 }
