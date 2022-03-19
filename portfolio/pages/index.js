389ff24cf8ee16a8dd4a3df4565909139d4e3185
Mizoko <mizoko320@gmail.com>
2022-03-19 09:02:58 +0900

    sassの導入

--- portfolio/pages/index.js
+++ portfolio/pages/index.js
@@ -1,6 +1,6 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import styles from '../styles/Home.module.sass'
 import Header from '../components/header'
 
 export default function Home() {
